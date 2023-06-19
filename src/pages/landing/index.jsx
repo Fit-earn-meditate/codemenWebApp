36e2ea69fb5fbd2891422387bf1b3402650d95d5
Divyanshu Tyagi <32699049+dvynshu95@users.noreply.github.com>
2023-06-20 02:05:48 +0530

    divyanshu | code clean up

---
+++ src/pages/landing/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="container min-h-[30rem]">
+      I am the landing Page!
+    </div>
+  )
+};
+
+
+export default Landing;
