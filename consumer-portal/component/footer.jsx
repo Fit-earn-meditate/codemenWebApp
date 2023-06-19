f5b5f87e3fb7824be6383e5c2c29c9c65ddc6a8d
Divyanshu Tyagi <32699049+dvynshu95@users.noreply.github.com>
2023-06-19 23:41:41 +0530

    divyanshu | Initial Project Setup

---
+++ consumer-portal/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        This is the Footer Element
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ consumer-portal/src/app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Header from '../../component/header'
+import Footer from '../../component/footer'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="">
+        This is the Codemen consumer portal
+      </main>
+      <Footer />
+    </>
+  )
+}
