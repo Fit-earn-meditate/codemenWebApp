6da72f74a87160b04cba74c614ed84326d2b9779
Divyanshu Tyagi <32699049+dvynshu95@users.noreply.github.com>
2023-06-20 00:24:51 +0530

    divyanshu | Project structure Strealined

---
+++ component/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer style={{background: "black", color: "white", padding:"24px", height: "500px"}}>
+      <span>
+        Logo
+      </span>
+      <nav>
+        <ul>
+          <li>Nav Link 1</li>
+        </ul>
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer;
