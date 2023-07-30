f2c3c076389b05a97c7ad8583ec934d104912796
Divyanshu Tyagi <32699049+dvynshu95@users.noreply.github.com>
2023-07-31 04:15:14 +0530

    added package lock file

--- modules/services/index.jsx
+++ modules/services/index.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Services = () => (
   <section id="services" className="container mx-auto py-20">
     <div className="text-center flex justify-center items-center flex-col">
-      <h2 className="text-5xl font-semibold">Services</h2>
+      <h2 className="text-5xl font-semibold text-[#01A165]">Services</h2>
       <div className="mt-2 px-20 text-[#7E7E7E] text-lg">
         <p>We Serve All Your Digital Needs</p>
       </div>
